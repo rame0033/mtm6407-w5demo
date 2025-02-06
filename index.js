e89991dd688ff79bec38be08d68feea0856467d4
Gian William Ramelb <144856637+rame0033@users.noreply.github.com>
2025-02-06 17:21:24 -0500

    Add function blocks

---
+++ index.js
@@ -0,0 +1,14 @@
+//Traditional function
+
+function showMessage() {
+//body or code block
+}
+
+//Arrow function
+
+const showGreeting = (name) => {
+//body or code block
+console.log(`Hello ${name}`);
+}
+
+showGreeting('John');
